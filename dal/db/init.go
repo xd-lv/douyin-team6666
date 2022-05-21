e25f4930b57b21ef5ee4042c11614af6fc2f6ebd
lxd <13011848392@wo.cn>
2022-05-21 17:15:24 +0800

    dal of user & video

---
+++ dal/db/init.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	gormopentracing "gorm.io/plugin/opentracing"
+	"main/constants"
+)
+
+var DB *gorm.DB
+
+// Init init DB
+func Init() {
+	var err error
+
+	DB, err = gorm.Open(mysql.Open(constants.MySQLDefaultDSN),
+		&gorm.Config{},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = DB.Use(gormopentracing.New()); err != nil {
+		panic(err)
+	}
+
+}
---
+++ dal/db/video_test.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	Init()
+	m.Run()
+}
+
+func TestUserCreate(t *testing.T) {
+	testVideo := &Video{
+		Author:   1,
+		PlayUrl:  "testPlayUrl",
+		CoverUrl: "testCoverUrl",
+		Title:    "testTitle",
+	}
+	ctx := context.Background()
+	CreateVideo(ctx, testVideo)
+}
+
+func TestGetVideo(t *testing.T) {
+	ctx := context.Background()
+	r, err := GetVideo(ctx, 1)
+	if err != nil {
+		println(err)
+	}
+	println(r.Author)
+}
+
+func TestListVideo(t *testing.T) {
+	ctx := context.Background()
+	r, err := ListVideo(ctx)
+	if err != nil {
+		println(err)
+	}
+	println(len(r))
+}
