68cb3c41824a0dbc89bda7a357a431fbc686541c
XQ-Gang <3035686788@qq.com>
2022-05-28 14:01:11 +0800

    feat(relation): handler RelationAction

--- controller/relation.go
+++ controller/relation.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main/pack"
+	"main/service/relationService"
 	"net/http"
 )
 
@@ -11,15 +12,43 @@ type UserListResponse struct {
 	UserList []pack.User `json:"user_list"`
 }
 
-// RelationAction no practical effect, just check if token is valid
-func RelationAction(c *gin.Context) {
-	token := c.Query("token")
+type ActionRequest struct {
+	Uid   int64 `form:"user_id" json:"user_id" binding:"required"`
+	ToUid int64 `form:"to_user_id" json:"to_user_id" binding:"required"`
+	Type  int32 `form:"action_type" json:"action_type" binding:"required"`
+}
 
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
-	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+const (
+	ActionFollow       = 1
+	ActionCancelFollow = 2
+)
+
+// RelationAction 关注与取消关注
+func RelationAction(c *gin.Context) {
+	action := ActionRequest{}
+	err := c.BindQuery(&action)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 2, StatusMsg: "Input parameter error"})
+		return
 	}
+
+	if action.Type == ActionFollow {
+		err := relationService.Follow(c, action.Uid, action.ToUid)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 3, StatusMsg: "redis server error"})
+			return
+		}
+	} else if action.Type == ActionCancelFollow {
+		err := relationService.CancelFollow(c, action.Uid, action.ToUid)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 3, StatusMsg: "redis server error"})
+			return
+		}
+	} else {
+		c.JSON(http.StatusOK, Response{StatusCode: 2, StatusMsg: "parameter 'action_type' error"})
+	}
+
+	c.JSON(http.StatusOK, Response{StatusCode: 0})
 }
 
 // FollowList all users have same follow list
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
 	github.com/appleboy/gin-jwt/v2 v2.8.0
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/minio/minio-go v6.0.14+incompatible
 	gorm.io/driver/mysql v1.3.3
 	gorm.io/gorm v1.23.5
@@ -13,6 +14,8 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ini/ini v1.66.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
