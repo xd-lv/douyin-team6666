b731631114335195737c6e5bba93d1d629e99948
XQ-Gang <3035686788@qq.com>
2022-05-30 20:45:20 +0800

    fix: fix typo

--- constants/errors.go
+++ constants/errors.go
@@ -3,7 +3,7 @@ package constants
 import "errors"
 
 var (
-	ErrInvalidParams = errors.New("Input parameter error")
+	ErrInvalidParams = errors.New("input parameter error")
 	ErrMysqlServer   = errors.New("mysql server error")
 	ErrRedisServer   = errors.New("redis server error")
 )
